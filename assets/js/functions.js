6847ce5f4f8f939dff30f0f186e13e8aad3c2865
ggoffy <gottfried.gabor@gmail.com>
2021-12-02 21:24:15 +0100

    clear

--- assets/js/functions.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$( "button, input:button, input:submit, input:file, input:reset" ).css("color","inherit").button();
-	$( ".check" ).css("color","#fff").button();
-	$( ".radio" ).css("color","#fff").buttonset();
-	$( ".toolbar" ).css("color","#000").buttonset();       
-});
